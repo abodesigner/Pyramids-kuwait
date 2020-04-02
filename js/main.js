a0759a319d4cdd9de0458d923f931a28484b194f
abodesigner <mohammed.abosalim@gmail.com>
2020-04-02 15:05:28 +0200

    adding files

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+  
+
+  // smooth scroll  
+  $("a[href^='#']").on("click", function(e){
+    e.preventDefault();
+    const target = $(this).attr('href');
+    $('html, body').animate({
+        scrollTop: $(target).offset().top}
+        , 800, 'linear');
+  });
+
+
+  // init slick slider
+  $('.our-clients').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,    
+  });
+
+
+});
